db055e6d10a0e44819663e83df2634f228813228
Albin Kjellson <albinkj91@yahoo.com>
2021-05-02 13:20:38 +0000

    Added RET opcode and implemented stack logic

--- memory.c
+++ memory.c
@@ -12,3 +12,18 @@ u_byte get_memory_value(u_short address){
 void set_memory_value(u_short address, u_byte value){
 	memory[address] = value;
 }
+
+void push_stack(u_short value, u_short *sp){
+	if(*sp > 0xfffe){
+		printf("ERROR::SP_OUT_OF_BOUNDS\n");
+		exit(0);
+	}
+	memory[*sp++] = value;
+	memory[*sp++] = value >> 8;
+}
+
+u_short pop_stack(u_short *sp){
+	u_short value = memory[--*sp] << 8;
+	value |= memory[--*sp];
+	return value;
+}
